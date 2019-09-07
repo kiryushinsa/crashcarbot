ff251a5b2021813845b49566b0550c6d42f44cf7
kiryushinsa <kiryushin.s.a@gmail.com>
2019-09-07 11:02:28 +0300

    added some new methods in bot class

--- src/main/java/org/crashcar/botapi/Bot.java
+++ src/main/java/org/crashcar/botapi/Bot.java
@@ -3,16 +3,20 @@ package org.crashcar.botapi;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-public class Bot extends  {
+public class Bot extends TelegramLongPollingBot {
     Logs logs= new Logs();
-
 
 
 
 
     @Override
     public void onUpdateReceived(Update update) {
-
+        if(update.hasMessage()&&update.getMessage().hasText())
+                {
+                    switch(update.getMessage().getText()){
+                        case "/start":{}
+                    }
+        }
     }
 
     @Override
